583b760958a7b8445690da450ee930a65e903d9e
Jisu An <ajs7270@image.cau.ac.kr>
2017-04-13 11:00:33 +0900

    add source code

---
+++ src/DoubleLinkedList.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+
+typedef struct node {
+	int data;
+	struct node* front;
+	struct node* rear;
+}Node;
+typedef Node* List;
+
+void push(int data,List* now);
+int stackPop(List* tail);
+int queuePop(List* head);
+
+int main(){
+	
+	int select;
+	int inputData;
+	List head = (Node*)malloc(sizeof(Node));	
+	List tail = head;
+	head->front = head;
+
+	printf("input an integer : ");
+	while ((inputData = getchar()) != '\n')
+		push(inputData-'0', &tail);
+	printf("Select Method (1) Stack (2) Queue : ");
+	scanf("%d", &select);	
+	
+	if (1 == select){
+		while (head != tail)
+			printf("%d\n", stackPop(&tail));
+
+	}
+	else if(2 == select){
+		while (head != tail)
+			printf("%d\n", queuePop(&head));
+	}
+	else{
+		printf("select error!\n");
+	}
+}
+
+void push(int data, List* now){
+	(*now)->rear = (Node*)malloc(sizeof(Node));
+	((*now)->rear)->front = *now;
+	((*now)->rear)->data = data;
+	*now = (*now)->rear;
+}
+
+int stackPop(List* tail){
+	int data = (*tail)->data;
+	Node* preNode = (*tail)->front;
+	
+	free(preNode->rear);
+
+	preNode->rear = NULL;
+	*tail = preNode;
+
+	return data;
+}
+
+int queuePop(List* head){
+	Node* nextNode = (*head)->rear;
+	int data = nextNode->data;
+
+	(*head)->rear = nextNode->rear;
+	if (nextNode)
+	(nextNode->rear)->front = *head;
+	
+	free(nextNode);
+
+	return data;
+}
